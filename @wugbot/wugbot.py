7cbece597670203eeac8290d53ff56687caf8edc
wugology <onna.nelson@gmail.com>
2015-01-31 17:48:35 -0800

    Wugbot Twitter Bot
    The code behind @wugbot’s retweeting.

---
+++ @wugbot/wugbot.py
@@ -0,0 +1,26 @@
+from twython import Twython
+import time, sys
+
+APP_KEY = 'xxx'
+APP_SECRET = 'yyy'
+OAUTH_TOKEN = 'zzz'
+OAUTH_TOKEN_SECRET = 'aaa'
+
+twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+bad_words = [" -RT"]
+good_words = ["wugbot", "computational linguist"]
+filter = " OR ".join(good_words)
+blacklist = " -".join(bad_words)
+keywords = filter + blacklist
+
+search_results = twitter.search(q=keywords, count=10)
+try:
+    for tweet in search_results["statuses"]:
+        try:
+            twitter.retweet(id = tweet["id_str"])
+        except TwythonError as e:
+            print e
+        time.sleep(500)
+except TwythonError as e:
+    print e
